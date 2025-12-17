abda4daae8d0b3929d136b39508c7ad061b09b05
Joaquim-Jambo <joaquimjambo12@gmail.com>
2025-12-17 12:55:32 +0100

    feat: add JWT token generation and verification functions

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,7 +1,15 @@
+import { Roles } from "../types/roles";
+
 export interface registerDTO {
     email: string;
     password: string;
     roleId: string;
     name: string;
     isActive?: boolean;
+}
+
+export interface tokenPayload {
+    userId: string,
+    email: string,
+    role: Roles
 }
