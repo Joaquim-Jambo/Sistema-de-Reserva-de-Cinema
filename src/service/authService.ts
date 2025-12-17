b146724e6b107e7fe829db374fd59044c2d19f81
Joaquim-Jambo <joaquimjambo12@gmail.com>
2025-12-17 17:05:58 +0100

    feat: add token generation and verification functions

---
+++ src/service/authService.ts
@@ -0,0 +1,23 @@
+import jwt, { type SignOptions } from "jsonwebtoken"
+import { tokenPayload } from "../src/models/userModel"
+
+export const generatedToken = (payload: tokenPayload) => {
+    const options: SignOptions = {
+        expiresIn: (process.env.JWT_EXPIRES_IN || '7d') as jwt.SignOptions['expiresIn']
+    };
+    const token = jwt.sign(
+        payload,
+        process.env.JWT_SECRET!,
+        options
+    );
+    return token;
+}
+
+export const verifyToken = (token: string): tokenPayload | undefined => {
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as tokenPayload;
+        return decoded;
+    } catch (error) {
+        console.error("Token invalido");
+    }
+}
