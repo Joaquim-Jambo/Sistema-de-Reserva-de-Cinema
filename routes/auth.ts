5dbbcfc50e2b9758c3e4cce383ac8a4b50271cfb
Joaquim-Jambo <joaquimjambo12@gmail.com>
2025-12-17 17:05:21 +0100

    refactor: remove unused authentication and validation code

--- routes/auth.ts
+++
@@ -1,7 +0,0 @@
-import express from "express"
-
-
-const authRoutes = express.Router();
-
-authRoutes.post('/register');
-authRoutes.post('/login');
--- src/schemas/clientSchema.ts
+++ src/schemas/clientSchema.ts
@@ -6,7 +6,6 @@ export const createClientSchema = createUserSchema.merge(z.object({
     phone: z.string().min(9, 'Deve ter no minimo 9 digitos').max(12, 'Deve ter no maximo 12 digitos'),
     dateOfBirth: z.string().regex(/^\d{2}\/\d{2}\/\d{4}$/, { message: 'Data de nascimento deve estar no formato DD/MM/YYYY' })
         .transform((val) => {
-            // Convert DD/MM/YYYY to ISO-8601
             const [day, month, year] = val.split('/');
             return new Date(`${year}-${month}-${day}`).toISOString();
         })
