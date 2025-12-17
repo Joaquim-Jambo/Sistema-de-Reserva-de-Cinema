a3ae9f725222ba25946e8eb5414bfe8c45fe8181
Joaquim-Jambo <joaquimjambo12@gmail.com>
2025-12-17 11:40:42 +0100

    feat: implement role-based access control middleware

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,5 +1,11 @@
-import { NextFunction, Request, Response } from "express"
+import type { NextFunction, Request, Response } from "express"
 import { z, ZodError } from "zod";
+import prisma from "../config/prismaClient";
+import { Roles } from "../types/roles";
+
+export interface AuthenticatedRequest extends Request {
+    userId?: string;
+}
 
 export function validateBody<T extends z.ZodTypeAny>(schema: T) {
     return (req: Request, res: Response, next: NextFunction) => {
@@ -14,6 +20,28 @@ export function validateBody<T extends z.ZodTypeAny>(schema: T) {
                 })
             }
             next(error)
+        }
+    }
+}
+
+export const checkRoles = (allowedRoles: Roles[]) => {
+    return async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+        try {
+            const userId = req.userId as string
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: userId
+                },
+                select: {
+                    role: true,
+                    roleId: true
+                }
+            })
+            if (!user || !allowedRoles.includes(user.role.role as Roles))
+                return res.status(403).json({ message: "Acesso negado" });
+
+        } catch (error) {
+            next(error);
         }
     }
 }
