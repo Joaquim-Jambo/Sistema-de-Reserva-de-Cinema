fddf5a966336a94b1ece357d797f9f43e32e8450
Joaquim-Jambo <joaquimjambo12@gmail.com>
2025-12-17 11:11:10 +0100

    fix: correct validation messages and refine password confirmation logic in user schema

--- schemas/userSchema.ts
+++ schemas/userSchema.ts
@@ -1,18 +1,18 @@
-import { email, z } from "zod"
+import { z } from "zod"
 
 export const createUserSchema = z.object({
-    email: z.string().email({ message: 'Email invalido' }),
+    email: z.string().email({ message: 'Email inválido' }),
     password: z.string()
-        .min(8, { message: "Senha deve ter no minimo 8 caracteres" })
-        .regex(/[a-z]/, { message: 'Senha deve conter pelo menos uma letra minuscula' })
-        .regex(/[A-Z]/, { message: 'Senha deve conter pelos menos uma letra maiscula' })
-        .regex(/[0-9]/, { message: 'Senha deve conter pelos menos um numero' })
-        .regex(/[^A-Za-z0-9]/, 'Deve conter pelo menos um caractere especial'),
-    roleId: z.uuid(),
+        .min(8, { message: "Senha deve ter no mínimo 8 caracteres" })
+        .regex(/[a-z]/, { message: 'Senha deve conter pelo menos uma letra minúscula' })
+        .regex(/[A-Z]/, { message: 'Senha deve conter pelo menos uma letra maiúscula' })
+        .regex(/[0-9]/, { message: 'Senha deve conter pelo menos um número' })
+        .regex(/[^A-Za-z0-9]/, { message: 'Senha deve conter pelo menos um caractere especial' }),
+    roleId: z.string().uuid({ message: 'ID de cargo inválido' }),
     isActive: z.boolean().default(true),
     confirmPassword: z.string()
-}).refine((data) => data.password !== data.confirmPassword, {
-    message: "As senhas nao coincidem",
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
     path: ['confirmPassword']
 })
 
