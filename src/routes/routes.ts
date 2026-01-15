e5ccb3bbf3e50f6f1e306123d658fe6c3e368837
Joaquim Jambo <joaquimjambo12@gmail.com>
2026-01-15 10:35:04 +0100

    feat: add session routes to main routing file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,11 +2,13 @@ import express from "express"
 import authRoutes from './auth';
 import adminRoutes from './admin';
 import clientRoutes from './client';
+import sessionRoutes from "./session";
 
 const routes = express()
 
 routes.use('/auth', authRoutes);
 routes.use('/admin', adminRoutes);
 routes.use('/clients', clientRoutes);
+routes.use('/sessions', sessionRoutes);
 
 export default routes;
