dba2993816920e55789743e0f69f3d4947abebb6
Joaquim Jambo <joaquimjambo12@gmail.com>
2026-01-16 16:07:03 +0100

    feat: update routing and schemas for categories and filters

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import adminRoutes from './admin';
 import clientRoutes from './client';
 import sessionRoutes from "./session";
 import categoriesRoutes from "./categories";
+import moviesRoutes from "./movies";
 
 const routes = express()
 
@@ -12,4 +13,5 @@ routes.use('/admin', adminRoutes);
 routes.use('/clients', clientRoutes);
 routes.use('/sessions', sessionRoutes);
 routes.use('/categories', categoriesRoutes)
+routes.use('/movies', moviesRoutes);
 export default routes;
