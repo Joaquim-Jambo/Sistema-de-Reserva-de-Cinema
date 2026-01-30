9a5dc8949e156ce484d42149d687189e2e5d6cea
Joaquim Jambo <joaquimjambo12@gmail.com>
2026-01-30 17:28:31 +0100

    feat: add update and delete movie functionality to movies routes

--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,12 +1,16 @@
 import express from "express"
-import { validateBody, validateQuery } from "../middlewares";
+import { validateBody, validateParams, validateQuery } from "../middlewares";
 import { createMovieSchema } from "../schemas/movieSchema";
-import { createMovieController, getAllMoviesController, getMoviesByFilterController } from "../controllers/movieController";
+import { createMovieController, deleteMovieController, getAllMoviesController, getMoviesByFilterController, updateMovieController } from "../controllers/movieController";
 import { filterSchema } from "../schemas";
+
 
 const moviesRoutes = express.Router();
 
 moviesRoutes.post('/', validateBody(createMovieSchema), createMovieController);
 moviesRoutes.get('/', validateQuery(filterSchema), getAllMoviesController);
+moviesRoutes.put('/:id', validateParams, validateBody, updateMovieController);
 moviesRoutes.get('/search', getMoviesByFilterController);
+moviesRoutes.delete('/:id', validateParams, deleteMovieController);
+
 export default moviesRoutes;
