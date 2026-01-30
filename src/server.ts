73dd73b0fd7c31df77938e9bd166a011c63ea33f
Joaquim Jambo <joaquimjambo12@gmail.com>
2026-01-30 17:27:46 +0100

    feat: add CORS middleware to server configuration for improved API security

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,17 @@ import dotenv from "dotenv"
 import { v2 as cloudinary } from 'cloudinary';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { uploadImage } from './service/uploadImageService';
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express()
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const port = process.env.PORT || 3000
